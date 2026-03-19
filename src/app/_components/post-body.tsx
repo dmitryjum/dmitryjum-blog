d630aff2968c225344121d1436b2425cba74b508
Dmitry Jum <xfiler@ymail.com>
2026-03-19 03:45:33 -0700

    improves article formatting

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="mx-auto max-w-[68rem]">
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
