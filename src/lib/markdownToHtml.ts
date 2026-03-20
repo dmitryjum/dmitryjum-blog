f9d7ece09a9f9e627e51162f1a434917a9f366bc
Dmitry Jum <xfiler@ymail.com>
2026-03-19 18:25:51 -0700

    color coats the code snippets in the blog articles

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,17 @@
 import { remark } from "remark";
-import html from "remark-html";
+import remarkRehype from "remark-rehype";
+import rehypeShiki from "@shikijs/rehype";
+import rehypeStringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(remarkRehype)
+    .use(rehypeShiki, {
+      theme: "monokai",
+      fallbackLanguage: "text",
+    })
+    .use(rehypeStringify)
+    .process(markdown);
+
   return result.toString();
 }
